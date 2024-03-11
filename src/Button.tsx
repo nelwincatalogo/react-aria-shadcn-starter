a560095804b5ad81dcc5e00ea1b299eccde9cb67
Nelwin Catalogo <catalogonelwin@gmail.com>
2024-03-11 16:33:52 +0800

    💄 button: update styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,25 +4,38 @@ import { tv } from 'tailwind-variants';
 import { focusRing } from './utils';
 
 export interface ButtonProps extends RACButtonProps {
-  variant?: 'primary' | 'secondary' | 'destructive' | 'icon'
+  variant?: 'default' | 'secondary' | 'destructive' | 'outline' | 'ghost' | 'link',
+  size?: 'default' | 'sm' | 'lg' | 'icon'
 }
 
 let button = tv({
   extend: focusRing,
-  base: 'px-5 py-2 text-sm text-center transition rounded-lg border border-black/10 dark:border-white/10 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] dark:shadow-none cursor-default',
+  base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
   variants: {
     variant: {
-      primary: 'bg-blue-600 hover:bg-blue-700 pressed:bg-blue-800 text-white',
-      secondary: 'bg-gray-100 hover:bg-gray-200 pressed:bg-gray-300 text-gray-800 dark:bg-zinc-600 dark:hover:bg-zinc-500 dark:pressed:bg-zinc-400 dark:text-zinc-100',
-      destructive: 'bg-red-700 hover:bg-red-800 pressed:bg-red-900 text-white',
-      icon: 'border-0 p-1 flex items-center justify-center text-gray-600 hover:bg-black/[5%] pressed:bg-black/10 dark:text-zinc-400 dark:hover:bg-white/10 dark:pressed:bg-white/20 disabled:bg-transparent'
+      default: 'bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90',
+      secondary:
+          'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
+      destructive:
+          'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
+      outline:
+          'border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+      ghost: 'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+      link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
     },
     isDisabled: {
       true: 'bg-gray-100 dark:bg-zinc-800 text-gray-300 dark:text-zinc-600 forced-colors:text-[GrayText] border-black/5 dark:border-white/5'
-    }
+    },
+    size: {
+      default: 'h-10 px-4 py-2',
+      sm: 'h-9 rounded-md px-3',
+      lg: 'h-11 rounded-md px-8',
+      icon: 'size-10',
+    },
   },
   defaultVariants: {
-    variant: 'primary'
+    variant: 'default',
+    size: 'default',
   }
 });
 
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,4 +1,6 @@
+import { ChevronRight } from 'lucide-react';
 import { Button } from '../src/Button';
+import React from 'react';
 
 export default {
   title: 'Button',
@@ -10,29 +12,65 @@ export default {
   argTypes: {
     variant: {
       control: 'select',
-      options: ['primary', 'secondary', 'destructive']
+      options: ['default', 'secondary', 'destructive', 'outline', 'ghost', 'link']
+    },
+    size: {
+      control: 'select',
+      options: ['default', 'sm', 'lg', 'icon']
     }
   },
   args: {
     isDisabled: false,
-    children: 'Button'
+    children: 'Button',
+    size: 'default',
   }
 };
 
-export const Primary = {
+export const Default = {
   args: {
-    variant: 'primary'
+    variant: 'default',
+    children: 'Default',
   },
 };
 
 export const Secondary = {
   args: {
-    variant: 'secondary'
+    variant: 'secondary',
+    children: 'Secondary',
   },
 };
 
 export const Destructive = {
   args: {
-    variant: 'destructive'
+    variant: 'destructive',
+    children: 'Destructive',
   },
 };
+
+export const Outline = {
+  args: {
+    variant: 'outline',
+    children: 'Outline',
+  },
+};
+
+export const Ghost = {
+  args: {
+    variant: 'ghost',
+    children: 'Ghost',
+  },
+};
+
+export const Link = {
+  args: {
+    variant: 'link',
+    children: 'Link',
+  },
+};
+
+export const Icon = {
+  args: {
+    variant: 'outline',
+    children: <ChevronRight className="h-4 w-4" />
+  }
+}
